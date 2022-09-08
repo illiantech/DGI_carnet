f3c7c0884b1f74295e630b8f80adecd6244f6986
KanuteGX <pimentel2002d@gmail.com>
2022-09-08 17:29:43 -0400

    black mode check

---
+++ js/front/black_mode.js
@@ -0,0 +1,33 @@
+// elementos
+const blackMode = document.getElementById('blackMode');
+const root = document.firstElementChild;
+
+// valid variable (true default for open black mode)
+let validMode;
+
+if (localStorage.getItem('validMode') === 'true') validMode = true;
+else validMode = false;
+
+const dinamicMode = () => {
+	blackMode.classList.toggle('black-mode__active', validMode);
+
+	if (validMode) {
+		root.style.setProperty('--LetBgDoc', 'hsl(0, 0%, 10%)');
+		root.style.setProperty('--LetColorPrimaryTwo', 'hsl(0, 0%, 95%)');
+
+		localStorage.setItem('validMode', validMode);
+		validMode = false;
+	} else {
+		root.style.setProperty('--LetBgDoc', 'hsl(0, 0%, 95%)');
+		root.style.setProperty('--LetColorPrimaryTwo', 'hsl(223, 49%, 23%)');
+
+		localStorage.setItem('validMode', validMode);
+		validMode = true;
+	}
+};
+
+dinamicMode();
+
+blackMode.addEventListener('click', () => {
+	dinamicMode();
+});
